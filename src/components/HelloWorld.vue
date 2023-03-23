c956212ac46b8f2b4ca15de9c5a81746b1a2d256
lairjack <15183323935@163.com>
2023-03-23 16:19:47 +0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { onMounted, ref } from 'vue'
+// import { Viewer } from 'cesium';
+import * as Cesium from 'cesium';
+onMounted(() => {
+  const dxt = new Cesium.UrlTemplateImageryProvider({
+    url: "https://t0.tianditu.gov.cn/mapservice/swdx?tk=0326461d186c59cafc7df93e69a814d8",
+    subdomains: ['0', '1', '2', '3', '4', '5', '6', '7'],
+    tilingScheme: new Cesium.WebMercatorTilingScheme(),
+    maximumLevel: 18
+  })
+  const viewer = new Cesium.Viewer('mapContainer', {
+    baseLayerPicker: false,
+    // imageryProvider: dxt
+  });
+
+})
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <div id="mapContainer">
+
+  </div>
+</template>
+
+<style scoped>
+#mapContainer {
+  widows: 100%;
+  height: 100%;
+}
+</style>
